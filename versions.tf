aac2134fc29a694a7990787c00092e4a4e536982
Denis O <denis.o@linux.com>
2021-04-28 20:37:48 +0300

    Fixed versions for terraform modules

--- versions.tf
+++ versions.tf
@@ -1,3 +1,22 @@
 terraform {
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.65.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.1.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.1.2"
+    }
+
+  }
+
   required_version = ">= 0.12"
 }
