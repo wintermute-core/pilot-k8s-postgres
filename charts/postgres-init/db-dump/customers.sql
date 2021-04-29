4c206b1a3e5a77d255afb7ecf7bb766783ec7237
Denis O <denis.o@linux.com>
2021-04-29 21:12:51 +0300

    Additional sql dump file

---
+++ charts/postgres-init/db-dump/customers.sql
@@ -0,0 +1,10 @@
+CREATE TABLE customers (
+   id integer,
+   first_name varchar(50),
+   last_name varchar(50),
+   notes text
+);
+
+INSERT INTO customers VALUES('1', 'Jhon', 'Doe', 'Test customer');
+INSERT INTO customers VALUES('2', 'Denis', 'O', 'Real customer');
+
